ced5fe2c839b1ccd1816cb4b827834dd0ab570f5
Wellington Vasconcelos <wltvasc@hotmail.com>
2022-07-25 14:43:42 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import br.com.wellington.desafio.dominio.Bootcamp;
 import br.com.wellington.desafio.dominio.Curso;
+import br.com.wellington.desafio.dominio.Dev;
 import br.com.wellington.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -21,8 +22,41 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devWellington = new Dev();
+        devWellington.setNome("Wellington");
+        devWellington.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Wellington" + devWellington.getConteudoInscritos());
+        devWellington.prodredir();
+        devWellington.prodredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos Wellington" + devWellington.getConteudoInscritos());
+        System.out.println("Conteudos Conluidos Wellington" + devWellington.getConteudoConcluido());
+        System.out.println("XP : " +devWellington.calcularTotalXp());
+
+
+
+        System.out.println("---------------");
+        Dev devCaroline = new Dev();
+        devCaroline.setNome("Carolene");
+        devCaroline.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Caroline" + devCaroline.getConteudoInscritos());
+        devCaroline.prodredir();
+        devCaroline.prodredir();
+        devCaroline.prodredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos Caroline" + devCaroline.getConteudoInscritos());
+        System.out.println("Conteudos Conluidos Caroline" + devCaroline.getConteudoConcluido());
+        System.out.println("XP : " +devCaroline.calcularTotalXp());
     }
 }
